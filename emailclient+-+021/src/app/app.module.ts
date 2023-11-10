59ae06ff4158e13e12dc00737344a49a6542f574
Simone Ramundi <simone.ramundi@4science.com>
2023-11-10 17:02:03 +0100

    first commit

---
+++ emailclient+-+021/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthModule} from './auth/auth.module';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ emailclient+-+021/src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface UsernameAvailableResponse {
+    available: boolean;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    constructor(private http: HttpClient) {
+    }
+
+    usernameAvailable(username: string) {
+        return this.http.post<UsernameAvailableResponse>(
+            'https://api.angular-email.com/auth/username',
+            {
+                username
+            }
+        );
+    }
+
+
+    signup(credentials: any){
+        return this.http.post<any>(
+            'https://api.angular-email.com/auth/signup',
+            credentials
+        );
+    }
+}
