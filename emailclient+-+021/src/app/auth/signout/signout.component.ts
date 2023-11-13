05e51f23d647665229938bf5d41d001e920ab9d6
Simone Ramundi <simone.ramundi@4science.com>
2023-11-13 17:13:14 +0100

    Signing out process

--- emailclient+-+021/src/app/auth/signout/signout.component.ts
+++ emailclient+-+021/src/app/auth/signout/signout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
@@ -7,12 +9,15 @@ import {AuthService} from "../auth.service";
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+      private authService: AuthService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
     this.authService.signout().subscribe(()=>{
       // Navigate user back to signin page
+      this.router.navigateByUrl('/');
     });
   }
-
 }
