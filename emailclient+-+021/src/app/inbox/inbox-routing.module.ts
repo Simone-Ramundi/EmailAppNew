b5c59b9c80b4faa70ddbaca5b2d79b228cf921e9
Simone Ramundi <simone.ramundi@4science.com>
2023-11-14 15:48:04 +0100

    Child Routing

--- emailclient+-+021/src/app/inbox/inbox-routing.module.ts
+++ emailclient+-+021/src/app/inbox/inbox-routing.module.ts
@@ -1,9 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {PlaceholderComponent} from "./placeholder/placeholder.component";
+import {EmailShowComponent} from "./email-show/email-show.component";
 
 const routes: Routes = [
-    {path: "", component: HomeComponent}
+    {
+        path: "",
+        component: HomeComponent,
+        children: [
+            { path: ':id', component: EmailShowComponent},
+            { path: '', component: PlaceholderComponent}
+        ]
+    }
 ];
 
 @NgModule({
