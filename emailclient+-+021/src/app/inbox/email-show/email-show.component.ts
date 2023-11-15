af8f5079ec840eb9962898379d4f8ade1230ab2c
Simone Ramundi <simone.ramundi@4science.com>
2023-11-15 11:35:43 +0100

    Email Resolver

--- emailclient+-+021/src/app/inbox/email-show/email-show.component.ts
+++ emailclient+-+021/src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {EmailService} from "../email.service";
-import {switchMap} from "rxjs/operators";
 import {Email} from "../email";
 
 @Component({
@@ -14,18 +12,12 @@ export class EmailShowComponent implements OnInit {
 
     constructor(
         private route: ActivatedRoute,
-        private emailService: EmailService
     ) {
+        this.email = route.snapshot.data.email;
+        this.route.data.subscribe(({email})=>{
+            this.email = email;
+        });
     }
 
-    ngOnInit() {
-        this.route.params.pipe(
-            switchMap(({id}) => {
-                return this.emailService.getEmail(id);
-            })
-        )
-            .subscribe(email => {
-                this.email = email;
-            });
-    }
+    ngOnInit() {}
 }
