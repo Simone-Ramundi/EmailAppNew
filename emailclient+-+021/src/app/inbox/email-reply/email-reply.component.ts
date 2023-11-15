4b9dd0d220414e127f02a60dfed60c7e9f2b3545
Simone Ramundi <simone.ramundi@4science.com>
2023-11-15 17:25:01 +0100

    Formatting reply emails

--- emailclient+-+021/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient+-+021/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {Email} from "../email";
 
 @Component({
@@ -7,12 +7,20 @@ import {Email} from "../email";
     styleUrls: ['./email-reply.component.css']
 })
 export class EmailReplyComponent implements OnInit {
-    showModal = false
-    email: Email
+    showModal = false;
+    @Input() email: Email;
     constructor() {
     }
 
     ngOnInit() {
+        const text = this.email.text.replace(/\n/gi, '\n> ');
+        this.email = {
+            ...this.email,
+            from: this.email.to,
+            to: this.email.from,
+            subject: `RE: ${this.email.subject}`,
+            text: `\n\n\n--------${this.email.from} wrote:\n> ${text}`
+        }
     }
     onSubmit(email:Email){
 
